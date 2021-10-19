963ed58c28d3a1e776985613cd86fbc17865776c
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-19 23:35:37 +0530

    Update linearserch.c

--- linearserch.c
+++ linearserch.c
@@ -1,16 +1,4 @@
 #include<stdio.h>
-
-int linearSerch(int a[],int size,int key){
-	int i=0,pos;
-	for(i=0;i<size;i++){
-	   if(a[i]==key){
-	   	pos=i;
-	   	printf("key found \n position :");
-	   	return pos+1;
-	   }	
-	}
-	return 0;
-}
 int main(){
 	int num,size,i;
 	printf("enter the size of array :");
@@ -19,12 +7,15 @@ int main(){
 	printf("enter the elements :\n");
 	for(i=0;i<size;i++){
 		scanf("%d",&arr[i]);
-	}	
-    
+	}
 	printf("enter the no to serch :");
 	scanf("%d",&num);
-	
-    int result=linearSerch(arr,8,num);
-    printf("%d",result);
+    int pos;
+	for(i=0;i<size;i++){
+	   if(a[i]==key){
+	   	pos=i;
+		   break;}}
+	   	printf("key found \n position :");
+    printf("%d",pos+1);
 
 }
